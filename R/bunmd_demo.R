fef629d2b1d906ca175f9158ee7aa5a7851d2bc4
Maria Osborne <mariaosborne@demog.berkeley.edu>
2021-11-15 11:21:35 -0800

    Added data documentation

---
+++ R/bunmd_demo.R
@@ -0,0 +1,17 @@
+#' Demo BUNMD Data Set
+#'
+#' A data set containing a sample of the CenSoc Berkeley Unified Numident Mortality Database (BUNMD) file,
+#' including age at death and select covariates.
+#'
+#
+#' @format A data frame with 81,002 rows and 6 variables:
+#' \describe{
+#'   \item{ssn}{Social security number}
+#'   \item{bpl_string}{Country of birth}
+#'   \item{death_age}{Age at death (integer years)}
+#'   \item{byear}{Calender year of birth}
+#'   \item{dyear}{Calender year of death}
+#'   \item{age_first_application}{Age at first social security application}
+#'
+#' }
+"bunmd_demo"
---
+++ R/censoc_example.R
@@ -0,0 +1,17 @@
+#' Example CenSoc Data Set
+#'
+#' A data set containing age at death and select covariates
+#' for sample for men born in 1910.
+#'
+#' @format A data frame with 100,000 rows and 6 variables:
+#' \describe{
+#'   \item{id}{Individual-level identifier}
+#'   \item{byear}{Calender year of birth}
+#'   \item{death_age}{Age at death (integer years)}
+#'   \item{educ_yrs}{Years of education completed}
+#'   \item{urban}{Residence in urban area in 1940 (0 = FALSE, 1 = TRUE)}
+#'   \item{ownhome}{Homeowner (0 = FALSE, 1 = TRUE)}
+#' }
+#'
+#'
+"censoc_example"
