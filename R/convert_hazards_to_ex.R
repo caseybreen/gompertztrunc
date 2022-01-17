58f1f5e5e4804bb60e466c627667c55601846566
Maria Osborne <mariaosborne@demog.berkeley.edu>
2022-01-16 23:14:20 -0800

    change calls to converto_ex

--- R/convert_hazards_to_ex.R
+++ R/convert_hazards_to_ex.R
@@ -1,21 +1,21 @@
-#' Convert hazard ratios to life expectancy at age 65
-#'
+#' Convert hazard ratios to differences in remaining life expectancy
+#' at a given age (defaults to age 65)
 #'
 #' @param df dataframe of results given by gompertz_mle() function
-#' @param lower lower age bound
-#' @param upper upper age bound
+#' @param age age at which to calculate remaining life expectancy
+#' @param upper_age maximal age to use in life table calculation
 #' @param M Gompertz parameter modal age at death
 #' @param b Gompertz mortality slope parameter
 #' @param use_model_estimates use estimates of the Gompertz Parameters from the model, rather than defaults
 #'
-#' @return A dataframe of hazards ratios and corresponding e65 estimates and confidence intervals
+#' @return A dataframe of hazards ratios and corresponding e(x) estimates and confidence intervals
 #'
 #'
 #' @export
 #'
 
 
-convert_hazards_to_ex <- function(df, lower = 65, upper = 120, M = 80, b = 0.075, use_model_estimates=F) {
+convert_hazards_to_ex <- function(df, age = 65, upper_age = 120, M = 80, b = 0.075, use_model_estimates=F) {
 
   # Use estimated mode and b from model, overriding any other input
   if(use_model_estimates) {
