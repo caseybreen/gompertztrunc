7ad862b0bdfadd88cb785d275a8f2ba25af33aba
caseybreen <caseyfbreen@gmail.com>
2022-02-20 10:41:37 -0800

    update vignette

---
+++ R/numident_demo.R
@@ -0,0 +1,17 @@
+#' Demo Numident dataset
+#'
+#' A data set containing a sample of the CenSoc-Numident file,
+#' including age at death and select covariates.
+#'
+#
+#' @format A data frame with 81,002 rows and 6 variables:
+#' \describe{
+#'   \item{ssn}{Social Security number}
+#'   \item{bpl_string}{Country of birth}
+#'   \item{death_age}{Age at death (integer years)}
+#'   \item{byear}{Calender year of birth}
+#'   \item{dyear}{Calender year of death}
+#'   \item{age_first_application}{Age at first Social Security application}
+#'
+#' }
+"numident_demo"
