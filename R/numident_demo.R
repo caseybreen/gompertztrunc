c3a50373501eef9e672b5acaf52db15b8d2f2ac7
caseybreen <caseyfbreen@gmail.com>
2022-02-20 20:34:29 -0800

    address a few cran warnings

--- R/numident_demo.R
+++ R/numident_demo.R
@@ -4,14 +4,38 @@
 #' including age at death and select covariates.
 #'
 #
-#' @format A data frame with 81,002 rows and 6 variables:
+#' @format A data frame with 62,899 rows and 30 variables:
 #' \describe{
-#'   \item{ssn}{Social Security number}
-#'   \item{bpl_string}{Country of birth}
-#'   \item{death_age}{Age at death (integer years)}
-#'   \item{byear}{Calender year of birth}
-#'   \item{dyear}{Calender year of death}
-#'   \item{age_first_application}{Age at first Social Security application}
+#' \item{histid}{Historical unique identiﬁer}
+#' \item{byear}{Year of birth}
+#' \item{bmonth}{Month of birth}
+#' \item{dyear}{Year of death}
+#' \item{dmonth}{Month of death}
+#' \item{death_age}{Age at death (years)}
+#' \item{zip_residence}{ZIP Code of residence at time of death}
+#' \item{weight}{CenSoc weight}
+#' \item{pernum}{Person number in sample unit}
+#' \item{perwt}{IPUMS person weight}
+#' \item{age}{Age in 1940}
+#' \item{sex}{Sex in 1940}
+#' \item{bpl}{Place of birth}
+#' \item{mbpl}{Mother’s place of birth}
+#' \item{fbpl}{Father’s place of birth}
+#' \item{educd}{Educational attainment (detailed)}
+#' \item{empstatd}{Employment status (detailed)}
+#' \item{hispan}{Hispanic/Spanish/Latino origin}
+#' \item{incnonwg}{Had non-wage/salary income over $50}
+#' \item{incwage}{Wage and salary income}
+#' \item{marst}{Marital status}
+#' \item{nativity}{Foreign birthplace or parentage}
+#' \item{occ}{Occupation}
+#' \item{occscore}{Occupational income score}
+#' \item{ownershp}{Ownership of dwelling (tenure)}
+#' \item{race}{Race}
+#' \item{rent}{Monthly contract rent}
+#' \item{serial}{Household serial number}
+#' \item{statefip}{State of residence 1940}
+#' \item{urban}{Urban/rural status}
 #'
 #' }
 "numident_demo"
